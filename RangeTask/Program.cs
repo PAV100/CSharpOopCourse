5a8e30775188f6df7e5784aa8fda61ce8ba415fc
PAV100 <109933596+PAV100@users.noreply.github.com>
2022-07-25 20:29:34 +0700

    Update Program.cs

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,7 +6,7 @@ namespace RangeTask
     {
         static void Main()
         {
-            Console.WriteLine("Hello World 5!");
+            Console.WriteLine("Hello World 6!");
         }
     }
 }
