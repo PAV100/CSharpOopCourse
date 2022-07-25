3b1f6ea678d993281b01c73bb1b4e7175a795f59
PAV100 <wr_me@mail.ru>
2022-07-25 20:02:41 +0700

    World 5

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,7 +6,7 @@ namespace RangeTask
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 5!");
         }
     }
 }
