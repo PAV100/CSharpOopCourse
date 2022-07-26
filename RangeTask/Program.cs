cf44f42ba438556b0df4eaf67c860f70d59b4668
PAV100 <wr_me@mail.ru>
2022-07-26 13:53:29 +0700

    Range*. Extended Range class with GetIntersection, GetUnion, GetDifference methods

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -6,51 +6,132 @@ namespace RangeTask
     {
         static void Main()
         {
-            Console.WriteLine("The program demonstrates initialisation and modification of Range class instances");
+            Console.WriteLine("The program demonstrates operations on sets of Range class instances");
             Console.WriteLine();
 
-            double from = -5; // 1. normal limits order 
-            double to = 5;
-            double testPoint = 0;
+            // 1. range2 does not overlap range1
+            Range range1 = new Range(10, 20);
+            Range range2 = new Range(0, 8);
+            Range rangesIntersection1 = range1.GetIntersection(range2);
+            Range[] rangesUnion1 = range1.GetUnion(range2);
+            Range[] rangesDifference1 = range1.GetDifference(range2);
 
-            Range range1 = new Range(from, to);
-            double length = range1.GetLength();
-            bool isInside = range1.IsInside(testPoint);
-            string isInsideText = isInside ? "is inside" : "is not inside";
-
-            Console.WriteLine("from = {0}, to = {1}, class instance: [{2}, {3}], length = {4}, test point {5} {6}",
-                from, to, range1.From, range1.To, length, testPoint, isInsideText);
-
-            range1.From = -7; // set new "from" value, without limits swaping
-            length = range1.GetLength();
-            isInside = range1.IsInside(testPoint);
-            isInsideText = isInside ? "is inside" : "is not inside";
-
-            Console.WriteLine("from = {0}, to = {1}, class instance: [{2}, {3}], length = {4}, test point {5} {6}",
-                from, to, range1.From, range1.To, length, testPoint, isInsideText);
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection1));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion1));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference1));
             Console.WriteLine();
 
-            from = 0; // 3. equal limits
-            to = 0;
-            testPoint = 0;
+            // 2. range2 touches range1 in one endpoint 
+            range2.To = 10;
+            Range rangesIntersection2 = range1.GetIntersection(range2);
+            Range[] rangesUnion2 = range1.GetUnion(range2);
+            Range[] rangesDifference2 = range1.GetDifference(range2);
 
-            Range range3 = new Range(from, to);
-            length = range3.GetLength();
-            isInside = range3.IsInside(testPoint);
-            isInsideText = isInside ? "is inside" : "is not inside";
-
-            Console.WriteLine("from = {0}, to = {1}, class instance: [{2}, {3}], length = {4}, test point {5} {6}",
-                from, to, range3.From, range3.To, length, testPoint, isInsideText);
-
-            range3.From = -0.01; // set new "from" and "to" values, without limits swaping
-            range3.To = -0.001;
-            length = range3.GetLength();
-            isInside = range3.IsInside(testPoint);
-            isInsideText = isInside ? "is inside" : "is not inside";
-
-            Console.WriteLine("from = {0}, to = {1}, class instance: [{2}, {3}], length = {4}, test point {5} {6}",
-                from, to, range3.From, range3.To, length, testPoint, isInsideText);
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection2));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion2));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference2));
             Console.WriteLine();
+
+            // 3. range2 partially overlaps range1
+            range2.To = 18;
+            Range rangesIntersection3 = range1.GetIntersection(range2);
+            Range[] rangesUnion3 = range1.GetUnion(range2);
+            Range[] rangesDifference3 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection3));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion3));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference3));
+            Console.WriteLine();
+
+            // 4. ranges have the same right endpoint
+            range2.To = 20;
+            Range rangesIntersection4 = range1.GetIntersection(range2);
+            Range[] rangesUnion4 = range1.GetUnion(range2);
+            Range[] rangesDifference4 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection4));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion4));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference4));
+            Console.WriteLine();
+
+            // 5. range1 inside range2
+            range2.To = 30;
+            Range rangesIntersection5 = range1.GetIntersection(range2);
+            Range[] rangesUnion5 = range1.GetUnion(range2);
+            Range[] rangesDifference5 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection5));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion5));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference5));
+            Console.WriteLine();
+
+            // 6. ranges are the same
+            range2.From = 10;
+            range2.To = 20;
+            Range rangesIntersection6 = range1.GetIntersection(range2);
+            Range[] rangesUnion6 = range1.GetUnion(range2);
+            Range[] rangesDifference6 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection6));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion6));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference6));
+            Console.WriteLine();
+
+            // 7. range2 inside range1
+            range2.From = 12;
+            range2.To = 18;
+            Range rangesIntersection7 = range1.GetIntersection(range2);
+            Range[] rangesUnion7 = range1.GetUnion(range2);
+            Range[] rangesDifference7 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection7));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion7));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference7));
+            Console.WriteLine();
+
+            // 8. ranges have the same left endpoint
+            range2.From = 10;
+            range2.To = 30;
+            Range rangesIntersection8 = range1.GetIntersection(range2);
+            Range[] rangesUnion8 = range1.GetUnion(range2);
+            Range[] rangesDifference8 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection8));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion8));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference8));
+            Console.WriteLine();
+
+            // 9. range2 partially overlaps range1
+            range2.From = 18;
+            Range rangesIntersection9 = range1.GetIntersection(range2);
+            Range[] rangesUnion9 = range1.GetUnion(range2);
+            Range[] rangesDifference9 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection9));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion9));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference9));
+            Console.WriteLine();
+
+            // 10.  // range2 touches range1 in an endpoint
+            range2.From = 20;
+            Range rangesIntersection10 = range1.GetIntersection(range2);
+            Range[] rangesUnion10 = range1.GetUnion(range2);
+            Range[] rangesDifference10 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection10));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion10));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference10));
+            Console.WriteLine();
+
+            // 11. range2 does not overlap range1
+            range2.From = 22;
+            Range rangesIntersection11 = range1.GetIntersection(range2);
+            Range[] rangesUnion11 = range1.GetUnion(range2);
+            Range[] rangesDifference11 = range1.GetDifference(range2);
+
+            Console.WriteLine("Intersection of {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesIntersection11));
+            Console.WriteLine("Union of        {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesUnion11));
+            Console.WriteLine("Difference of   {0} and {1} is {2}", Range.ToString(range1), Range.ToString(range2), Range.ToString(rangesDifference11));
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RangeTask
 {
@@ -26,6 +22,80 @@ namespace RangeTask
         public bool IsInside(double number)
         {
             return (number >= From) && (number <= To); // (number >= from) && (number <= to)
+        }
+
+        public Range GetIntersection(Range range)
+        {
+            if (range.From >= To || range.To <= From)
+            {
+                return null;
+            }
+
+            return new Range(Math.Max(From, range.From), Math.Min(To, range.To));
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            if (range.From > To)
+            {
+                return new Range[] { new Range(From, To), new Range(range.From, range.To) };
+            }
+
+            if (range.To < From)
+            {
+                return new Range[] { new Range(range.From, range.To), new Range(From, To) };
+            }
+
+            return new Range[] { new Range(Math.Min(From, range.From), Math.Max(To, range.To)), null };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            if (range.From <= From && range.To >= To)
+            {
+                return null;
+            }
+
+            if (range.From >= To || range.To <= From)
+            {
+                return new Range[] { new Range(From, To), null };
+            }
+
+            if (range.From > From && range.To < To)
+            {
+                return new Range[] { new Range(From, range.From), new Range(range.To, To) };
+            }
+
+            if (range.From > From)
+            {
+                return new Range[] { new Range(From, range.From), null };
+            }
+
+            return new Range[] { new Range(range.To, To), null };
+        }
+
+        public static string ToString(Range range)
+        {
+            if (range is null)
+            {
+                return "null";
+            }
+
+            return string.Format("({0}, {1})", range.From, range.To);
+        }
+        public static string ToString(Range[] rangesArray)
+        {
+            if (rangesArray is null)
+            {
+                return "null";
+            }
+
+            if (rangesArray[1] is null)
+            {
+                return Range.ToString(rangesArray[0]);
+            }
+
+            return Range.ToString(rangesArray[0]) + "U" + Range.ToString(rangesArray[1]);
         }
     }
 }
