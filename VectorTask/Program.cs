6ae005d0756904bc9d2968ee87da3d3a0c3d3893
PAV100 <wr_me@mail.ru>
2022-07-26 19:18:43 +0700

    Vector. Autoproperty and 4 constructors done.

---
+++ VectorTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VectorTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program demonstrates Vector class functionality");
+            Console.WriteLine();
+
+            Vector vector1 = new Vector(3);
+            Vector vector2 = new Vector(vector1);
+            Vector vector3 = new Vector(new double[] { 4, 5, 6 });
+            Vector vector4 = new Vector(3, new double[] { 7, 8 });
+
+            Console.WriteLine("vector1 ({0}, {1}, {2})", vector1.Components[0], vector1.Components[1], vector1.Components[2]);
+            Console.WriteLine("vector2 ({0}, {1}, {2})", vector2.Components[0], vector2.Components[1], vector2.Components[2]);
+            Console.WriteLine("vector3 ({0}, {1}, {2})", vector3.Components[0], vector3.Components[1], vector3.Components[2]);
+            Console.WriteLine("vector4 ({0}, {1}, {2})", vector4.Components[0], vector4.Components[1], vector4.Components[2]);
+            Console.WriteLine();
+
+            vector1.Components = new double[] { 1, 2, 3 };
+
+            Console.WriteLine("vector1 ({0}, {1}, {2})", vector1.Components[0], vector1.Components[1], vector1.Components[2]);
+            Console.WriteLine("vector2 ({0}, {1}, {2})", vector2.Components[0], vector2.Components[1], vector2.Components[2]);
+            Console.WriteLine("vector3 ({0}, {1}, {2})", vector3.Components[0], vector3.Components[1], vector3.Components[2]);
+        }
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace VectorTask
+{
+    internal class Vector
+    {
+        public double[] Components { get; set; }
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Number of components must be > 0", nameof(n));
+            }
+
+            Components = new double[n];
+        }
+
+        public Vector(Vector vector)
+        {
+            Components = vector.Components;
+        }
+
+        public Vector(double[] components)
+        {
+            if (components.Length == 0)
+            {
+                throw new ArgumentException("Array length must be > 0", nameof(components));
+            }
+
+            Components = components;
+        }
+
+        public Vector(int n, double[] components)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Number of components must be > 0", nameof(n));
+            }
+
+            if (components.Length == 0)
+            {
+                throw new ArgumentException("Array length must be > 0", nameof(components));
+            }
+
+            if (n <= components.Length)
+            {
+                Components = components;
+            }
+            else
+            {
+                Components = new double[n];
+
+                for (int i = 0; i < components.Length; i++)
+                {
+                    Components[i] = components[i];
+                }
+            }
+        }
+
+
+    }
+}
