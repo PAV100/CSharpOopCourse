50437c6a1a0bf9c01611c886205f9d43eb0c2ece
PAV100 <wr_me@mail.ru>
2022-07-27 23:24:27 +0700

    Vector. Almost everything is done, but overriding equals and hashCode

--- VectorTask/Program.cs
+++ VectorTask/Program.cs
@@ -12,7 +12,7 @@ namespace VectorTask
             Vector vector1 = new Vector(3);
             Vector vector2 = new Vector(vector1);
             Vector vector3 = new Vector(new double[] { 4, 5, 6, 0 });
-            Vector vector4 = new Vector(6, new double[] { 7, 8 });
+            Vector vector4 = new Vector(5, new double[] { 7, 8 });
 
             Console.WriteLine("vector1 {0}, dim {1}, len {2}", vector1.ToString(), vector1.GetSize(), vector1.GetLength());
             Console.WriteLine("vector2 {0}, dim {1}, len {2}", vector2.ToString(), vector2.GetSize(), vector2.GetLength());
@@ -28,11 +28,28 @@ namespace VectorTask
             vector3.SubtractVector(vector4);
             Console.WriteLine("vector3 {0}, dim {1}, len {2}", vector3.ToString(), vector3.GetSize(), vector3.GetLength());
 
-            vector3.MultiplyByScalar(-1.1257);
+            vector3.MultiplyByScalar(-2);
             Console.WriteLine("vector3 {0}, dim {1}, len {2}", vector3.ToString(), vector3.GetSize(), vector3.GetLength());
 
             vector3.ReverseVector();
             Console.WriteLine("vector3 {0}, dim {1}, len {2}", vector3.ToString(), vector3.GetSize(), vector3.GetLength());
+
+            Console.WriteLine("{0}, {1}, {2}", vector3.GetComponent(0), vector3.GetComponent(1), vector3.GetComponent(2));
+
+            vector3.SetComponent(0, 10);
+            vector3.SetComponent(2, -10);
+            Console.WriteLine("vector3 {0}, dim {1}, len {2}", vector3.ToString(), vector3.GetSize(), vector3.GetLength());
+
+            //vector1.Equals(vector2);
+
+            Vector vector5 = Vector.AddVectors(vector3, vector4);
+            Console.WriteLine("vector5 {0}, dim {1}, len {2}", vector5.ToString(), vector5.GetSize(), vector5.GetLength());
+
+            Vector vector6 = Vector.SubtractVectors(vector3, vector1);
+            Console.WriteLine("vector6 {0}, dim {1}, len {2}", vector6.ToString(), vector6.GetSize(), vector6.GetLength());
+
+            Console.WriteLine("{0}, {1}, {2}", 
+                Vector.GetScalarProduct(vector1, vector3), Vector.GetScalarProduct(vector3, vector4), Vector.GetScalarProduct(vector3, vector5));
         }
     }
 }
--- VectorTask/Vector.cs
+++ VectorTask/Vector.cs
@@ -2,6 +2,7 @@
 
 namespace VectorTask
 {
+    /// <summary>Provides vectors of dimention n with real-number components and static and non-static methods for operations with vectors</summary>
     internal class Vector
     {
         public double[] Components { get; set; }
@@ -61,16 +62,18 @@ namespace VectorTask
             }
         }
 
+        /// <summary>Gets vector dimention</summary>        
         public int GetSize()
         {
             return this.Components.Length;
         }
 
-        public string ToString()
+        public override string ToString()
         {
             return "{" + String.Join(", ", this.Components) + "}";
         }
 
+        /// <summary>Adds a vector to a current vector. Returns modified vector</summary>
         public Vector AddVector(Vector vector)
         {
             if (this.Components.Length >= vector.Components.Length)
@@ -95,6 +98,7 @@ namespace VectorTask
             return this;
         }
 
+        /// <summary>Subtracts a vector from a current vector. Returns modified vector</summary>
         public Vector SubtractVector(Vector vector)
         {
             if (this.Components.Length >= vector.Components.Length)
@@ -106,11 +110,11 @@ namespace VectorTask
             }
             else
             {
-                Vector vectorsDifference = new Vector(vector);
+                Vector vectorsDifference = new Vector(vector.ReverseVector());
 
                 for (int i = 0; i < this.Components.Length; i++)
                 {
-                    vectorsDifference.Components[i] = this.Components[i] - vectorsDifference.Components[i];
+                    vectorsDifference.Components[i] += this.Components[i];
                 }
 
                 this.Components = vectorsDifference.Components;
@@ -119,6 +123,7 @@ namespace VectorTask
             return this;
         }
 
+        /// <summary>Multiplies a current vector by a scalar. Returns modified vector</summary>
         public Vector MultiplyByScalar(double value)
         {
             for (int i = 0; i < this.Components.Length; i++)
@@ -129,6 +134,7 @@ namespace VectorTask
             return this;
         }
 
+        /// <summary>Multiplies a current vector by -1. Returns modified vector</summary>
         public Vector ReverseVector()
         {
             for (int i = 0; i < this.Components.Length; i++)
@@ -151,17 +157,108 @@ namespace VectorTask
             return Math.Sqrt(squaresSum);
         }
 
-        /*public double GetComponent(int componentIndex)
+        public double GetComponent(int componentIndex)
         {
-            if (componentIndex <= 0)
+            int componentsLength = this.Components.Length;
+
+            if (componentIndex < 0 || componentIndex >= componentsLength)
             {
-                throw new ArgumentException("Number of components must be > 0", nameof(componentIndex));
+                throw new ArgumentException("componentIndex must be between 0 and vector size", nameof(componentIndex));
             }
 
-            Components = new double[componentIndex];
+            return this.Components[componentIndex];
+        }
 
-            return 0;
+        public void SetComponent(int componentIndex, double component)
+        {
+            int componentsLength = this.Components.Length;
+
+            if (componentIndex < 0 || componentIndex >= componentsLength)
+            {
+                throw new ArgumentException("componentIndex must be between 0 and vector size", nameof(componentIndex));
+            }
+
+            this.Components[componentIndex] = component;
+        }
+
+        /*public override bool Equals(object obj)
+        {
+            if (obj is Vector vector)
+            { 
+
+            }
+
+                return Name == person.Name;
+            
+
+            return false;
         }*/
 
+        public static Vector AddVectors(Vector vector1, Vector vector2)
+        {
+            Vector majorVector;
+            Vector minorVector;
+
+            if (vector1.Components.Length >= vector2.Components.Length)
+            {
+                majorVector = vector1;
+                minorVector = vector2;
+            }
+            else
+            {
+                majorVector = vector2;
+                minorVector = vector1;
+            }
+
+            Vector vectorsSum = new Vector(majorVector);
+
+            for (int i = 0; i < minorVector.Components.Length; i++)
+            {
+                vectorsSum.Components[i] += minorVector.Components[i];
+            }
+
+            return vectorsSum;
+        }
+
+        public static Vector SubtractVectors(Vector vector1, Vector vector2)
+        {
+            Vector minorVector;
+            Vector vectorsDifference;
+            int negationMultiplier;
+
+            if (vector1.Components.Length >= vector2.Components.Length)
+            {
+                minorVector = vector2;
+                vectorsDifference = new Vector(vector1);
+                negationMultiplier = -1;
+            }
+            else
+            {
+                minorVector = vector1;
+                vectorsDifference = new Vector(vector2.ReverseVector());
+                negationMultiplier = 1;
+            }
+
+            for (int i = 0; i < minorVector.Components.Length; i++)
+            {
+                vectorsDifference.Components[i] += minorVector.Components[i] * negationMultiplier;
+            }
+
+            return vectorsDifference;
+        }
+
+        public static double GetScalarProduct(Vector vector1, Vector vector2)
+        {
+            int maxIndex = Math.Min(vector1.Components.Length, vector2.Components.Length);
+
+            double scalarProduct = 0;
+
+            for (int i = 0; i < maxIndex; i++)
+            {
+                scalarProduct += vector1.Components[i] * vector2.Components[i];
+            }
+
+            return scalarProduct;
+        }
     }
 }
