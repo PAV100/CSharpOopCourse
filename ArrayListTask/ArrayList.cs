7d024c641794880d7dcfbb904a1e5a4733e1c5ee
PAV100 <wr_me@mail.ru>
2023-02-14 01:42:28 +0700

    ArrayList. Release 1.0. for review

--- ArrayListTask/ArrayList.cs
+++ ArrayListTask/ArrayList.cs
@@ -1,91 +1,334 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System.Text;
 
 namespace ArrayListTask
 {
     internal class ArrayList<T> : IList<T>
     {
-        private T[] items = new T[8];
+        private const int InitialCapacity = 4;
+
+        private T[] items;
+
+        private int capacity;
+
+        private int count;
+
+        private int modificationsCount;
 
         public T this[int index]
         {
-            get { return items[index]; }
-            set { items[index] = value; }
+            get
+            {
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("List is empty. It is impossible to refer a value");
+                }
+
+                if (index < 0 || index >= count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Item index = {index}, but it must be >= 0 and < {count}");
+                }
+
+                return items[index];
+            }
+
+            set
+            {
+                if (count == 0)
+                {
+                    throw new InvalidOperationException("List is empty. It is impossible to refer a value");
+                }
+
+                if (index < 0 || index >= count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Item index = {index}, but it must be >= 0 and < {count}");
+                }
+
+                if (IsReadOnly)
+                {
+                    throw new InvalidOperationException("List is read only. It is impossible to make changes");
+                }
+
+                items[index] = value;
+            }
         }
 
-        public int Count => throw new NotImplementedException();
+        public int Capacity
+        {
+            get { return capacity; }
 
-        public bool IsReadOnly => throw new NotImplementedException();
+            set
+            {
+                if (IsReadOnly)
+                {
+                    throw new InvalidOperationException("List is read only. It is impossible to make changes");
+                }
+
+                if (value < count)
+                {
+                    throw new ArgumentException($"Capacity must be >= Count ({count})", nameof(value));
+                }
+
+                if (value != capacity)
+                {
+                    Array.Resize(ref items, value);
+                }
+
+                capacity = value;
+            }
+        }
+
+        int ICollection<T>.Count
+        {
+            get { return count; }
+        }
+
+        public bool IsReadOnly { get; set; }
 
         public ArrayList()
         {
+            items = new T[0];
         }
 
         public ArrayList(T item)
         {
+            items = new T[InitialCapacity];
+            capacity = InitialCapacity;
+            items[0] = item;
+            count = 1;
         }
 
         public ArrayList(int capacity)
         {
+            items = new T[capacity];
+            this.capacity = capacity;
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("List is read only. It is impossible to make changes");
+            }
+
+            if (count >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[count] = item;
+            count++;
+            modificationsCount++;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("List is read only. It is impossible to make changes");
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                items[i] = default;
+            }
+
+            count = 0;
+            modificationsCount++;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            foreach (T e in items)
+            {
+                if (e.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            if (count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException($"Destination array is too short to place {count} list item(s). It must be {count + arrayIndex} element(s) length.", nameof(array) + ", " + nameof(arrayIndex));
+            }
+
+            Array.Copy(items, 0, array, arrayIndex, count);
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            int modificationsCountCopy = modificationsCount;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (modificationsCountCopy != modificationsCount)
+                {
+                    throw new InvalidOperationException("Item(s) were added/deleted to/from a list during iteration");
+                }
+
+                yield return items[i];
+            }
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            if (count == 0)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("List is read only. It is impossible to make changes");
+            }
+
+            if (index < 0 || index > count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Item index = {index}, but it must be >= 0 and <= {count}");
+            }
+
+            if (count >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            for (int i = count; i > index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+
+            items[index] = item;
+
+            count++;
+            modificationsCount++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("List is read only. It is impossible to make changes");
+            }
+
+            if (count == 0)
+            {
+                return false;
+            }
+
+            int index = -1;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+
+            return true;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("List is read only. It is impossible to make changes");
+            }
+
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Item index = {index}, but it must be >= 0 and < {count}");
+            }
+
+            for (int i = index; i < count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            items[count - 1] = default;
+            count--;
+            modificationsCount++;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
 
-        public void TrimExcess(T list)
+        public void TrimExcess()
         {
-            throw new NotImplementedException();
+            double listFillingLevel = 0.9;
+
+            if (count < listFillingLevel * items.Length)
+            {
+                for (int i = count; i < items.Length; i++)
+                {
+                    items[i] = default;
+                }
+
+                capacity = count;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("items = [");
+
+            if (count == 0)
+            {
+                sb.Append("  ");
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(items[i]).Append(", ");
+            }
+
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(']');
+            sb.Append(", capacity = ").Append(capacity);
+            sb.Append(", count = ").Append(count);
+            sb.Append(", IsReadOnly = ").Append(IsReadOnly);
+            return sb.ToString();
+        }
+
+        private void IncreaseCapacity()
+        {
+            int increasedLength = items.Length == 0 ? InitialCapacity : items.Length * 2;
+
+            Array.Resize(ref items, increasedLength);
+            capacity = increasedLength;
         }
     }
 }
--- ArrayListTask/Program.cs
+++ ArrayListTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace ArrayListTask
@@ -7,16 +8,114 @@ namespace ArrayListTask
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The program demonstrates homemade class of generic list on an array: ArrayList<T> : IList<T>");
+            Console.WriteLine();
 
+            Console.WriteLine("1. Constructor without arguments creates empty list");
             ArrayList<int> list1 = new ArrayList<int>();
-            list1[0] = 100;
-            int i = list1[0];
-            Console.WriteLine("i = {0}", i);
+            Console.WriteLine("List: {0}", list1.ToString());
+            Console.WriteLine();
 
-            List<int> list2 = new List<int>();
-            list2.Add(0);
+            Console.WriteLine("2. Add to the list some items with Add() method");
+            list1.Add(100);
+            list1.Add(200);
+            list1.Add(300);
+            Console.WriteLine("List: {0}", list1.ToString());
+            Console.WriteLine();
 
+            Console.WriteLine("3. Method Contains() checkes if the list has a given value");
+            Console.WriteLine("List contains {0} = {1}", 200, list1.Contains(200));
+            Console.WriteLine("List contains {0} = {1}", 500, list1.Contains(500));
+            Console.WriteLine();
+
+            Console.WriteLine("4. Lets create an empty array and use CopyTo() method to fill it with the list items");
+            int[] array = new int[4];
+            Console.WriteLine("array = [{0}]", string.Join(", ", array));
+            list1.CopyTo(array, 1);
+            Console.WriteLine("array = [{0}]", string.Join(", ", array));
+            Console.WriteLine();
+
+            Console.WriteLine("5. Two enumerators created and the list is enumerated by both");
+            IEnumerator list1Enumerator = list1.GetEnumerator();
+            while (list1Enumerator.MoveNext())
+            {
+                Console.WriteLine(list1Enumerator.Current);
+            }
+            Console.WriteLine();
+
+            IEnumerator<int> list1Enumerator2 = list1.GetEnumerator();
+            while (list1Enumerator2.MoveNext())
+            {
+                Console.WriteLine(list1Enumerator2.Current);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("6. Method IndexOf() is shown");
+            Console.WriteLine("List: {0}", list1.ToString());
+            int value = 300;
+            Console.WriteLine("A value {0} is in the list at index {1}", value, list1.IndexOf(value));
+            Console.WriteLine();
+
+            Console.WriteLine("7. Constructor creates a list with one item");
+            ArrayList<int> list2 = new ArrayList<int>(item: 90);
+            Console.WriteLine("List: {0}", list2.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("8. Method Insert() allows to add some items");
+            list2.Insert(0, 50);
+            list2.Insert(1, 60);
+            list2.Insert(2, 70);
+            list2.Insert(3, 80);
+            Console.WriteLine("List: {0}", list2.ToString());
+            Console.WriteLine();
+
+            value = 80;
+            Console.WriteLine("9. Method Remove() deletes first occurance of an item {0}", value);
+            Console.WriteLine("Removed = {0}, List: {1}", list2.Remove(value), list2.ToString());
+            Console.WriteLine();
+
+            value = 1;
+            Console.WriteLine("10. Method RemoveAt() deletes an item at index {0}", value);
+            list2.RemoveAt(1);
+            Console.WriteLine("List: {0}", list2.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("11. Method Clear() deletes all items");
+            list2.Clear();
+            Console.WriteLine("List: {0}", list2.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("12. Constructor creates an empty list of given capacity");
+            ArrayList<int> list3 = new ArrayList<int>(capacity: 10);
+            Console.WriteLine("List: {0}", list3.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("13. Fill the list up to 80% level and use method TrimExess()");
+            list3.Insert(0, 20);
+            list3.Insert(0, 19);
+            list3.Insert(0, 18);
+            list3.Insert(0, 17);
+            list3.Insert(0, 16);
+            list3.Insert(0, 15);
+            list3.Insert(0, 14);
+            list3.Insert(0, 13);
+            //list3.Insert(0, 12);
+            Console.WriteLine("List: {0}", list3.ToString());
+            list3.TrimExcess();
+            Console.WriteLine("List: {0}", list3.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("14. Changing list capacity with Capacity property");
+            list3.Capacity = 15;
+            Console.WriteLine("List: {0}", list3.ToString());
+            list3.Capacity = 12;
+            Console.WriteLine("List: {0}", list3.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("15. Changing list IsReadOnly property");
+            list3.IsReadOnly = true;
+            Console.WriteLine("List: {0}", list3.ToString());
+            //list3.RemoveAt(0);
         }
     }
 }
