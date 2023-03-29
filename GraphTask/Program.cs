d6dde8bb451328c62ffc2b1e9b5618cc5bc64707
PAV100 <wr_me@mail.ru>
2023-03-29 16:26:18 +0700

    Graph. Release 1.0. for review

--- GraphTask/Program.cs
+++ GraphTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace GraphTask
 {
@@ -6,19 +8,33 @@ namespace GraphTask
     {
         static void Main()
         {
-            const int GraphVerticesCount = 20;
+            const int GraphVerticesCount = 25;
 
             Console.WriteLine("Program demonstrates graph traversal algorithms");
             Console.WriteLine();
 
-            bool[] visited1 = new bool[GraphVerticesCount];
             int[,] graph1 = new int[GraphVerticesCount, GraphVerticesCount];
             GenerateGraph(graph1);
+
+            Console.WriteLine("1. \"Big\" graph with random edges");
             PrintGraphToConsole(graph1);
+
+            Console.Write("Breadth first traversal: ");
+            foreach (int e in GetBreadthFirstTraversalEnumerator(graph1))
+            {
+                Console.Write($"{e} ");
+            }
             Console.WriteLine();
 
-             
-            bool[] visited2 = new bool[6];
+            Console.Write("Depth first traversal  : ");
+            foreach (int e in GetDepthFirstTraversalEnumerator(graph1))
+            {
+                Console.Write($"{e} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+
             int[,] graph2 =
             {
                 { 0, 1, 0, 0, 0, 0 },
@@ -28,9 +44,168 @@ namespace GraphTask
                 { 0, 0, 0, 1, 0, 0 },
                 { 0, 0, 0, 0, 0, 0 }
             };
+
+            Console.WriteLine("2. \"Small\" graph with manually set edges");
             PrintGraphToConsole(graph2);
 
+            Console.Write("Breadth first traversal: ");
+            foreach (int e in GetBreadthFirstTraversalEnumerator(graph2))
+            {
+                Console.Write($"{e} ");
+            }
+            Console.WriteLine();
 
+            Console.Write("Depth first traversal  : ");
+            foreach (int e in GetDepthFirstTraversalEnumerator(graph2))
+            {
+                Console.Write($"{e} ");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through graph vertices using breadth-first traversal
+        /// </summary>
+        /// <returns>The sequence of graph vertices number values</returns>
+        public static IEnumerable GetBreadthFirstTraversalEnumerator(int[,] graphAdjacencyMatrix)
+        {
+            if (graphAdjacencyMatrix is null)
+            {
+                yield break;
+            }
+
+            if (graphAdjacencyMatrix.Rank != 2 || graphAdjacencyMatrix.GetLength(0) != graphAdjacencyMatrix.GetLength(1))
+            {
+                throw new ArgumentException("Graph must be specified as 2D square array (adjacency matrix)", nameof(graphAdjacencyMatrix));
+            }
+
+            if (graphAdjacencyMatrix.GetLength(0) == 0)
+            {
+                yield break;
+            }
+
+            bool[] visitedVertices = new bool[graphAdjacencyMatrix.GetLength(0)];
+
+            while (true)
+            {
+                int firstUnvisitedVertex = GetFirstUnvisitedVertex(visitedVertices);
+
+                if (firstUnvisitedVertex != -1)
+                {
+                    Queue<int> queue = new();
+                    queue.Enqueue(firstUnvisitedVertex);
+
+                    while (queue.Count > 0)
+                    {
+                        int currentVertex = queue.Dequeue();
+
+                        if (visitedVertices[currentVertex])
+                        {
+                            continue;
+                        }
+
+                        visitedVertices[currentVertex] = true;
+
+                        for (int i = 0; i < graphAdjacencyMatrix.GetLength(0); i++)
+                        {
+                            if (visitedVertices[i])
+                            {
+                                continue;
+                            }
+
+                            if (graphAdjacencyMatrix[currentVertex, i] != 0)
+                            {
+                                queue.Enqueue(i);
+                            }
+                        }
+
+                        yield return currentVertex;
+                    }
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through graph vertices using depth-first traversal
+        /// </summary>
+        /// <returns>The sequence of graph vertices number values</returns>
+        public static IEnumerable GetDepthFirstTraversalEnumerator(int[,] graphAdjacencyMatrix)
+        {
+            if (graphAdjacencyMatrix is null)
+            {
+                yield break;
+            }
+
+            if (graphAdjacencyMatrix.Rank != 2 || graphAdjacencyMatrix.GetLength(0) != graphAdjacencyMatrix.GetLength(1))
+            {
+                throw new ArgumentException("Graph must be specified as 2D square array (adjacency matrix)", nameof(graphAdjacencyMatrix));
+            }
+
+            if (graphAdjacencyMatrix.GetLength(0) == 0)
+            {
+                yield break;
+            }
+
+            bool[] visitedVertices = new bool[graphAdjacencyMatrix.GetLength(0)];
+
+            while (true)
+            {
+                int firstUnvisitedVertex = GetFirstUnvisitedVertex(visitedVertices);
+
+                if (firstUnvisitedVertex != -1)
+                {
+                    Stack<int> stack = new();
+                    stack.Push(firstUnvisitedVertex);
+
+                    while (stack.Count > 0)
+                    {
+                        int currentVertex = stack.Pop();
+
+                        if (visitedVertices[currentVertex])
+                        {
+                            continue;
+                        }
+
+                        visitedVertices[currentVertex] = true;
+
+                        for (int i = graphAdjacencyMatrix.GetLength(0) - 1; i >= 0; i--)
+                        {
+                            if (visitedVertices[i])
+                            {
+                                continue;
+                            }
+
+                            if (graphAdjacencyMatrix[currentVertex, i] != 0)
+                            {
+                                stack.Push(i);
+                            }
+                        }
+
+                        yield return currentVertex;
+                    }
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        public static int GetFirstUnvisitedVertex(bool[] visitedVertices)
+        {
+            for (int i = 0; i < visitedVertices.Length; i++)
+            {
+                if (!visitedVertices[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         static void PrintGraphToConsole(int[,] graph)
